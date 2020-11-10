7758c34268b4ba7ec3eba0da9c50b818cfc4aa97
Artur Coelho <artur.temporal@hotmail.com>
2020-11-10 17:45:24 -0300

    função auxiliar de busca

--- tree_operators.h
+++ tree_operators.h
@@ -7,10 +7,13 @@
 int tree_is_empty(avl_t *t);
 
 // insere uma chave em um nodo apontado, retorna 1 em caso de sucesso, 0 ao contrário
-int insert_key_by_node(node_t *top, node_t *nd, key_t key);
+int insert_key_by_node(node_t **top, node_t *nd, key_t key);
 
 // insere uma chave no nodo especificado, com o pai
 int insert_key(node_t **nd, node_t *top, key_t key);
+
+// 
+int search_key_by_node(node_t *nd, key_t key);
 
 
 #endif
