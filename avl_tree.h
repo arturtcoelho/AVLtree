ee9ef5fc2475e0e2b9376c711d4bb1f7d018179b
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-11-10 17:02:16 -0300

    docs: Refatora comentarios

--- avl_tree.h
+++ avl_tree.h
@@ -17,22 +17,22 @@ struct avl {
 };
 typedef struct avl avl_t;
 
-// inicializa a árvore, necessário para iniciar operações, retorna 0 em caso de erro e !0 ao contrário
+// inicializa a árvore, necessário para iniciar operações, retorna 0 em caso de erro e !0 caso contrário
 int initialize_avl(avl_t *t);
 
-// insere uma chave key_t na árvore, retorna 0 em caso de erro e !0 ao contrário
+// insere uma chave key_t na árvore, retorna 0 em caso de erro e !0 caso contrário
 int insert_key_avl(avl_t *t, key_t key);
 
-// procura uma chave na árvore e insere seu valor em key, retorna 0 em caso de erro e !0 ao contrário
+// procura uma chave na árvore e retorna seu valor em key, retorna 0 em caso de erro e !0 caso contrário
 int search_key_avl(avl_t *t, key_t *key);
 
-// imprime a árvore na saída padrão, retorna 0 em caso de erro e !0 ao contrário
+// imprime a árvore na saída padrão, retorna 0 em caso de erro e !0 caso contrário
 int print_tree_avl(avl_t *t);
 
-// remove uma chave da árvore, retorna 0 em caso de erro e !0 ao contrário
+// remove uma chave da árvore, retorna 0 em caso de erro e !0 caso contrário
 int remove_key_avl(avl_t *t, key_t key);
 
-// destroi a árvore, retorna 0 em caso de erro e !0 ao contrário
+// destroi a árvore, retorna 0 em caso de erro e !0 caso contrário
 int destroy_tree_avl(avl_t *t);
 
 #endif
