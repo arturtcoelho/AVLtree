c17e1b61d726cdf437d1061a73f9fa5a9d829b66
Artur Coelho <artur.temporal@hotmail.com>
2020-11-17 12:40:09 -0300

    add: tratamento de input em myavl.py

--- myavl.py
+++ myavl.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
 
-# declara o objeto libc, para podermos utilizar diretamente funções padrões de C
-import ctypes
-avl = ctypes.CDLL("./avl_module.so")
+from avl_bridge import avl_tree
+tree = avl_tree()
 
-# avl._initialize_avl();
-# print(avl.avl_teste())
-# avl.avl_soma_teste(1, 3)
-print(avl.teste_avl())
+def input_exists(s):
+    return len(s) > 0
+
+def input_format(s):
+    if (len(s) < 2):
+        return 0
+
+    sl = slice(2)
+    comm = s[sl]
+    if (comm != "i " and comm != "r "):
+        return 0
+
+    sl = slice(2, len(s))
+    try:
+        n = int(s[sl])
+    except ValueError:
+        return 0
+
+    if (comm == "i "): # input na árvore 
+        tree.insert_key(n)
+    else: # remove da árvore
+        tree.remove_key(n)
+
+    return 1
+
+###################################################
+try:
+    comm = input()
+    while (input_exists(comm)):
+        if (not input_format(comm)):
+            print("erro de input")
+        comm = input()
+except EOFError:
+    pass
+
+tree.print_tree_parenthesis()
+
