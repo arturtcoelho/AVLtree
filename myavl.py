a2c7be0547cc8a9825db1b297ba7bc929731314d
Gabriel Nascarella Hishida <ielnascarella@hotmail.com>
2020-11-17 12:50:19 -0300

    refactor: Adiciona bloco __main__

--- myavl.py
+++ myavl.py
@@ -6,25 +6,21 @@ tree = avl_tree()
 def input_exists(s):
     return len(s) > 0
 
-# trata o input
 def input_format(s):
     if (len(s) < 2):
         return 0
 
-    # comando da entrada
     sl = slice(2)
     comm = s[sl]
     if (comm != "i " and comm != "r "):
         return 0
 
-    # número da entrada
     sl = slice(2, len(s))
     try:
         n = int(s[sl])
     except ValueError:
         return 0
 
-    # operação desejada
     if (comm == "i "): # input na árvore 
         tree.insert_key(n)
     else: # remove da árvore
@@ -32,18 +28,14 @@ def input_format(s):
 
     return 1
 
-###################################################
-
-# leitura dos dados na entrada padrão
-try:
-    comm = input()
-    while (input_exists(comm)):
-        if (not input_format(comm)):
-            print("erro de input")
+if __name__ == '__main__':
+    try:
         comm = input()
-except EOFError:
-    pass
+        while (input_exists(comm)):
+            if (not input_format(comm)):
+                print("erro de input")
+            comm = input()
+    except EOFError:
+        pass
 
-# com o input completo, imprime a árvore
-tree.print_tree_parenthesis()
-
+    tree.print_tree_parenthesis()
