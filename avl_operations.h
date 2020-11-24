fc8e0665c6cd95034dfd59335ee6bf9e96bf91e0
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-11-24 12:01:25 -0300

    docs: Reordena e ajusta descricao de funcoes

--- avl_operations.h
+++ avl_operations.h
@@ -12,13 +12,12 @@
 // retorna o fator de balanceamento da subárvore
 int balance_factor(node_t *nd);
 
+// rotaciona a árvore para realizar seu balanço
 int rotate_right(node_t **nd);
 int rotate_left(node_t **nd);
-
-// rotaciona a árvore para realizar seu balanço
 int rotate(node_t **nd);
 
-// rotaciona a árvore para realizar seu balanço
+// ajusta o fator de balanceamento nos nodos ascendentes
 int adjust_bf_upwards(node_t *nd);
 
 // desde root, confere e ajusta o balanço de cada nodo até uma chave específica
